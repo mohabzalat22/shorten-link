017bb432c522f07d113c245387a6ca785f8d465a
mohabzalat <mohabzalat20@gmail.com>
2024-12-15 17:46:37 +0200

    adding redirect route for web.

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LinkController;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::group(['prefix' => 'shorten', 'controller'=> LinkController::class], function(){
+    
+    Route::get('/redirect/{url}', 'redirect');
+
 });
